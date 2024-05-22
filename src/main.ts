87788191ae8fb6a8ae312896b262a996ca0de383
Pedro Durand <webpedrod@gmail.com>
2024-05-22 18:08:52 -0300

    chore: config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors();
+  process.env.PORT ? await app.listen(process.env.PORT) : await app.init();
+
+  module.exports = server;
 }
+
 bootstrap();
